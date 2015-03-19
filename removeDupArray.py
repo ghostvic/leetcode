403d8fe7343ce627cd4368dae5338e9fa3963874
Victor Zhang <victoryzsl@gmail.com>
2015-03-19 11:03:12 +0800

    removeDupArray.py

---
+++ removeDupArray.py
@@ -0,0 +1,36 @@
+'''
+Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this in place with constant memory.
+
+For example,
+Given input array A = [1,1,2],
+
+Your function should return length = 2, and A is now [1,2].
+'''
+
+#This function is not accepted by the system, but I leave it here.
+
+class Solution:
+    # @param a list of integers
+    # @return an integer
+    def removeDuplicates(self, A):
+        if len(A) == 1:
+            return len(A)
+        elif len(A) <= 0:
+            return -1
+        else:
+            remain = len(A)
+            i = 1
+            while remain > 0:
+                if A[i] == A[i-1]:
+                    A.pop(i)
+                    if len(A) > 1:
+                        remain = len(A) - i
+                    else:
+                        return len(A)
+                else:
+                    i += 1
+                    remain = len(A) - i
+
+            return len(A)
