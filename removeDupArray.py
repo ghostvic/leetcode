bcfccb5e834c7f2240466fce0c940e90799bde84
Victor Zhang <victoryzsl@gmail.com>
2015-06-12 12:37:11 -0400

    Update removeDupArray.py

--- removeDupArray.py
+++ removeDupArray.py
@@ -9,7 +9,34 @@ Given input array A = [1,1,2],
 Your function should return length = 2, and A is now [1,2].
 '''
 
-#This function is not accepted by the system, but I leave it here.
+'''
+The key point for this AC algrithm is that we have to move the element with differnt value to the right place : nums[length].
+So introducing the var length is key.
+'''
+
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer}
+    def removeDuplicates(self, nums):
+        if len(nums) == 0:
+            return -1
+        elif len(nums) == 1:
+            return 1
+        
+        index = 1
+        length = 1
+        tmp = nums[0]
+            
+        while index < len(nums):
+            if tmp != nums[index]:
+                tmp = nums[index]
+                nums[length] = nums[index]
+                length += 1
+            index += 1
+        
+        return length
+
+#The function below is not accepted by the system beacuse I used list.pop(), but I leave it here.
 
 class Solution:
     # @param a list of integers
